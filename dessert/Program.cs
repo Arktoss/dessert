d3bdd060ac2da257c921001d76a487faa31a63d7
Arktoss <111780704+Arktoss@users.noreply.github.com>
2023-09-07 12:30:28 -0400

    Add project files.

---
+++ dessert/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dessert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dessert_Choice();
+            Console.ReadKey();
+        }
+        static void Dessert_Choice()
+        {
+            Console.WriteLine("Would you like some dessert? ");
+
+
+            if string("yes")
+            {
+                Console.WriteLine("Awsome. I'll grab you a dessert menu");
+                Console.ReadKey();
+            }
+
+            else if ("no")
+            {
+                Console.WriteLine("No Worries. I'll grab you the check");
+                Console.ReadKey();
+            }
+        }
+    }
+}
